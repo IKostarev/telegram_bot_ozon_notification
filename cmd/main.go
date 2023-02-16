940b87c4c9326faff82168adab85e17a42538126
IKostarev <ilya.kostarev.9@gmail.com>
2023-02-16 14:46:59 +0500

    test echo bot

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"telegram_bot_ozon_notification/config"
+	"telegram_bot_ozon_notification/pkg"
 )
 
 func main() {
 	token := config.Token()
+	pkg.TakeAPIToTelegram(token)
 
 	fmt.Println("Programm is started!!!")
-	fmt.Println("Token = ", token)
 }
--- pkg/telegram.go
+++ pkg/telegram.go
@@ -1,5 +1,32 @@
 package pkg
 
-func TakeAPIToTelegram() {
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+)
 
+func TakeAPIToTelegram(token string) {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
